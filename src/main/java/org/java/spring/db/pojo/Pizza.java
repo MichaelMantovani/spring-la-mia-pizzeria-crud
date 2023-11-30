c5bc5c570db3552d983f48a32ae17aa4da0d6e29
MichaelMantovani <michmantovani@gmail.com>
2023-11-30 14:42:40 +0100

    add PizzaRepository

--- src/main/java/org/java/spring/db/pojo/Pizza.java
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -1,10 +1,14 @@
 package org.java.spring.db.pojo;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "pizza")
 public class Pizza {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
