67e5e0e1865f534e387cd706173b06ef48a07a90
MichaelMantovani <michmantovani@gmail.com>
2023-11-30 17:04:56 +0100

    add pizza detail page

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
 	
 	@Autowired PizzaService pizzaService;
+	
+	
 	@GetMapping
 	public String getPizza(Model model) {
 		List<Pizza> pizze = pizzaService.findAll();
@@ -20,4 +23,14 @@ public class MainController {
 		
 		return "home";
 	}
+	
+	@GetMapping("/pizza/{id}")
+	public String getPizzaDetail(Model model, @PathVariable int id) {
+		model.getAttribute("id");
+		Pizza pizza = pizzaService.findById(id);
+		model.addAttribute("pizza", pizza);
+		return "pizzaDetail";
+	}
+	
+	
 }
