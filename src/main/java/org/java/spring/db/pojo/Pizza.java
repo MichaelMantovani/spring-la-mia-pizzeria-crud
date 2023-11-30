a339697c8f3d834ada7f78997e28794542f5c9cc
MichaelMantovani <michmantovani@gmail.com>
2023-11-30 14:36:21 +0100

    init

---
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -0,0 +1,82 @@
+package org.java.spring.db.pojo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(length = 60, unique = true, nullable = false)
+	private String nome;
+
+	@Column(columnDefinition = "TEXT")
+	private String descrizione;
+
+	@Column()
+	private String foto;
+
+	@Column(nullable = false)
+	private double prezzo;
+
+	public Pizza() {
+	}
+
+	public Pizza(int id, String nome, String descrizione, String foto, double prezzo) {
+		setId(id);
+		setNome(nome);
+		setDescrizione(descrizione);
+		setFoto(foto);
+		setPrezzo(prezzo);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Pizza: " + this.nome + "\n" + this.descrizione + "\n" + this.prezzo;
+	}
+
+}
